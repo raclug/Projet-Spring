c950c3e5cf7bf60473ba7df0760e441a9649ff4f
807910 <807910@D-PROD-BP118804.prodesp-sede.local>
2017-03-08 16:54:45 -0300

    teste

--- src/main/java/br/com/alura/application/Application.java
+++ src/main/java/br/com/alura/application/Application.java
@@ -2,4 +2,9 @@ package br.com.alura.application;
 
 public class Application {
 
+	
+	private String variavel2;
+	
+	
+	
 }
---
+++ src/main/java/br/com/alura/application/Classe2.java
@@ -0,0 +1,5 @@
+package br.com.alura.application;
+
+public class Classe2 {
+
+}
